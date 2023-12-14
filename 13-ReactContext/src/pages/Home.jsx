db12b0aad467b32a2645954cb2adf4e3b3cc72ba
JuanSRovira <narukankid@gmail.com>
2023-12-13 23:40:21 -0600

    Home Page Basic(really basic)

---
+++ 13-ReactContext/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='izquierdo'>
+        <h2>Izquierdo</h2>
+      </div>
+      <div className='derecho'>
+        <h2>Derecho</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
