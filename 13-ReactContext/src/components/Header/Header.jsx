cf019e3d01bd136fd0937805df040fa39c8a499b
JuanSRovira <narukankid@gmail.com>
2023-12-27 22:04:58 -0600

    modified:   13-ReactContext/src/components/Header/Header.jsx 	modified:   13-ReactContext/src/pages/home/Home.jsx

--- 13-ReactContext/src/components/Header/Header.jsx
+++ 13-ReactContext/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import './header.css'
+import { useSongContext } from '../hooks/useSongContext'
 
 const Header = () => {
+  const { selectedSong } = useSongContext()
   return (
-    <div>Header</div>
+    <div className='header_container'>
+      {
+        selectedSong.title
+          ? (
+            <span>Now Playing: {selectedSong.title} - {selectedSong.artist}</span>
+            )
+          : <h2>Selecciona una Canción</h2>
+      }
+    </div>
   )
 }
 
--- 13-ReactContext/src/pages/home/Home.jsx
+++ 13-ReactContext/src/pages/home/Home.jsx
@@ -2,22 +2,22 @@ import Songlist from '../../components/SongList/Songlist'
 import SongDetails from '../../components/SongDetails/SongDetails'
 import { SongProvider } from '../../contexts/SongContext'
 import './home.css'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
     <SongProvider>
+      <Header />
       <div className='home-container'>
         <div className='izquierdo'>
+          <h2>Song List</h2>
           <Songlist />
-          <h1>Lado Izquierdo</h1>
         </div>
         <div className='derecho'>
           <SongDetails />
-          <h2>Lado Derecho</h2>
         </div>
       </div>
     </SongProvider>
   )
 }
-
 export default Home
