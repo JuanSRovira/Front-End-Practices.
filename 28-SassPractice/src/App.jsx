40c8acac4e8b59428e72fee5c3d0e04dad5c497c
JuanSRovira <narukankid@gmail.com>
2023-12-28 21:19:06 -0600

    Estructura Base

--- 28-SassPractice/src/App.jsx
+++ 28-SassPractice/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Home, Dashboard, Login, Secret, SignUp } from '@/pages'
+import Header from './components/Header/Header'
 
 function App () {
   return (
     <>
+      <Header />
       <h1>React App</h1>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <SignUp />
+
     </>
   )
 }
---
+++ 28-SassPractice/src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from './Home'
+import Dashboard from './Dashboard'
+import SignUp from './SignUp'
+import Login from './Login'
+import Secret from './Secret'
+
+export {
+  Dashboard,
+  Home,
+  SignUp,
+  Login,
+  Secret
+}
